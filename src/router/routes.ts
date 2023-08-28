7b52c3972bf63ae88b53baec9e52644e74f7dc05
Gedeon Isezerano <isezeranog@gmail.com>
2023-08-28 09:25:48 -0600

    updated route.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,8 +3,8 @@ import { RouteRecordRaw } from "vue-router";
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }]
+    component: () => import("layouts/AppLayout.vue")
+    // children: [{ path: "", component: () => import("pages/IndexPage.vue") }]
   },
 
   // Always leave this as last one,
