72d9c7cfa97d42cc8ae92c8c43d2dbb75025a941
HuntFeng <spacecurvature@gmail.com>
2024-09-20 22:00:26 -0600

    fix typo in files

--- src/backend/database/migration/index.ts
+++ src/backend/database/migration/index.ts
@@ -1,5 +1,5 @@
 import { moveProjectMetaToProjectFolder } from "./v0.13.0tov0.14.0";
-import { changeFolderIdToCategoryPath } from "./v0.16.3tov0.17.0";
+import { changeFolderIdToCategoryPath } from "./v0.17.0tov0.18.0";
 
 export async function migrate() {
   await moveProjectMetaToProjectFolder();
