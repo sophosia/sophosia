e992b415fee3c3737e65d351670911f7a4748053
HuntFeng <spacecurvature@gmail.com>
2025-01-05 14:21:53 -0600

    fixed tests

--- src/components/pdfreader/RightMenu.cy.ts
+++ src/components/pdfreader/RightMenu.cy.ts
@@ -4,7 +4,6 @@ describe("<RightMenu />", () => {
   it("renders", function () {
     cy.mount(RightMenu);
 
-    cy.dataCy("tab-meta-info").should("be.visible");
     cy.dataCy("tab-toc").should("be.visible");
     cy.dataCy("tab-annot-list").should("be.visible");
   });
